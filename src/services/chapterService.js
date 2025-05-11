056153c9d5138718c384edcfd76ac94775bbb267
Freeboku <theosoubeyrand18@gmail.com>
2025-05-11 23:57:27 +0200

    Update chapterService.js

--- src/services/chapterService.js
+++ src/services/chapterService.js
@@ -63,7 +63,7 @@ export const getChapterWithPages = async (chapterId, currentUserId, currentUserR
     return { 
       id: chapterData.id, 
       webtoonId: chapterData.webtoon_id, 
-      webtoonSlug: chapterData.webtoon?.id,
+      webtoonSlug: chapterData.webtoon_id,
       number: chapterData.number, 
       accessDenied: true, 
       required_roles: chapterData.required_roles || [], 
