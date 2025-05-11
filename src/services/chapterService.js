4d7c24bea63de4eeb775375337b6de7b870b5977
Freeboku <theosoubeyrand18@gmail.com>
2025-05-12 00:09:30 +0200

    Update chapterService.js

--- src/services/chapterService.js
+++ src/services/chapterService.js
@@ -27,7 +27,7 @@ const mapChapterData = (chapterData) => {
     id: chapterData.id,
     number: chapterData.number,
     webtoonId: chapterData.webtoon_id,
-    webtoonSlug: chapterData.webtoons?.slug,
+    webtoonSlug: chapterData.webtoon?.slug,
     webtoonTitle: chapterData.webtoons?.title || '',
     webtoonShowPublicViews: chapterData.webtoons?.show_public_views || false,
     thumbnailUrl: thumbnailUrl,
